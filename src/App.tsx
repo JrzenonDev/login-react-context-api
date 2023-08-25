67436032239b6550bd3618fc72a8d05dea019067
JrzenonDev <jrobertoonb@gmail.com>
2023-08-25 11:31:42 -0300

    feat: configure route protected in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import "./App.css";
+import { ProtectedLayout } from "./components/ProtectedLayout";
+import { AuthProvider } from "./context/AuthProvider";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="profile"
+            element={
+              <ProtectedLayout>
+                <h2>Olá, seja bem-vindo ao profile</h2>
+              </ProtectedLayout>
+            }
+          />
+          <Route path="login" />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
