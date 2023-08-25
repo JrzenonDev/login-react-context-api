fb842e716dc297734e9b38cb0e161e7f7ee3cd52
JrzenonDev <jrobertoonb@gmail.com>
2023-08-25 12:34:41 -0300

    feat: create function onFinish to login and fetch api use auth context

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,6 +1,20 @@
-import { Button, Col, Form, Input, Row } from "antd";
+import { Button, Col, Form, Input, Row, message } from "antd";
+import { useAuth } from "../../context/AuthProvider/useAuth";
+import { useNavigate } from "react-router-dom";
 
 export function Login() {
+  const auth = useAuth();
+  const navigate = useNavigate();
+
+  async function onFinish(values: { email: string; password: string }) {
+    try {
+      await auth.authenticate(values.email, values.password);
+      navigate("/profile");
+    } catch (error) {
+      message.error("Invalid email or password");
+    }
+  }
+
   return (
     <Row
       justify="center"
@@ -14,7 +28,7 @@ export function Login() {
           name="basic"
           labelCol={{ span: 8 }}
           wrapperCol={{ span: 16 }}
-          onFinish={() => {}}
+          onFinish={onFinish}
         >
           <Form.Item label="Email" name="email">
             <Input />
